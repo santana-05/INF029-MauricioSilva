de9d64c8bf67ff7d5ad588632df7af66aabe16e6
Santana <mauriciosantanadasilva123@gmail.com>
2025-05-28 20:32:57 +0000

    .

--- Trabalho1/MauricioSilva.h
+++ Trabalho1/MauricioSilva.h
@@ -43,7 +43,7 @@
 
 // int somar(int x, int y); //função utilizada para testes
 // int fatorial(int x); //função utilizada para testes
-int q1(char data[]);
+// int q1(char data[]);
 // DiasMesesAnos q2(char datainicial[], char datafinal[]);
 
 int q3(char *texto, char c, int isCaseSensitive);
